3bc4329e44ede89f5e3fea5c4fcda3e5facd64b9
P. Sehan Devinda <sehandevinda1@gmail.com>
2024-07-24 10:51:42 +0530

    complete welcome notice and seachbar

--- components/common/header/ScreenHeaderBtn.jsx
+++ components/common/header/ScreenHeaderBtn.jsx
@@ -7,7 +7,14 @@ import styles from './screenheader.style'
 const ScreenHeaderBtn = ({iconUrl, dimension, handlePress}) => {
   return (
     <View>
-      
+      <TouchableOpacity style= {styles.btnContainer} onPress={handlePress}>
+      <Image
+        source={iconUrl}
+        resizeMode="cover"
+        style={styles.btnImg(dimension)}
+        
+      />
+    </TouchableOpacity>
     </View>
   )
 }
--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -13,12 +13,59 @@ import styles from "./welcome.style";
 import { icons, SIZES } from "../../../constants";
 import { useRouter } from "expo-router";
 
+const jobsType = ["Full-time", "Part-time", "Contractor"]
+
 const Welcome = () => {
   const router = useRouter();
+  const [ActiveJobType, setActiveJobType] = useState("Full-time");
 
   return (
     <View>
-      
+      <View style={styles.container}>
+        <Text style={styles.userName}>Hello Sehan</Text>
+        <Text style={styles.welcomeMessage}>Find Your perfect job</Text>
+      </View>
+
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+          {/* <Image source={icons.search} style={styles.searchIcon} /> */}
+          <TextInput
+            style={styles.searchInput}
+            value=""
+            onChange={() => {}}
+            placeholder="What are you looking for?"
+          />
+        </View>
+
+        <TouchableOpacity style={styles.searchBtn} onPress={() => {}}>
+          <Image 
+          source={icons.search} 
+          resizeMode="contain"
+          style={styles.searchBtnImage} 
+          />
+        </TouchableOpacity>
+      </View>
+
+<View style={styles.tabsContainer}>
+  <FlatList
+  data = {jobsType}
+  renderItem={({item}) => (
+    <TouchableOpacity 
+    style={styles.tab(ActiveJobType, item)} 
+    onPress={() => {
+      setActiveJobType(item);
+      router.push(`/search/${item}`)
+    }}
+    >
+        <Text style={styles.tabText(ActiveJobType,item)} >{item}</Text>
+    </TouchableOpacity>
+  )}
+  keyExtractor={item => item}
+  contentContainerStyle = {{columnGap : SIZES.small}}
+  horizontal
+  />
+</View>
+
     </View>
   );
 };
